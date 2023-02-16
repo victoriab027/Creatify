f34dc67c137e5ceb7023187b25e1032e6041b9eb
victoriab027 <70912962+victoriab027@users.noreply.github.com>
2023-02-15 23:57:49 -0600

    nlp research

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Creatify',
+      version='1.0',
+      description='Creatify Project',
+      version = '3.7.0',
+      author='Victoria Black',
+      author_email='creatify.site@gmail.com',
+      packages=['spotipy','nltk','svgling','textblob'],
+     )
