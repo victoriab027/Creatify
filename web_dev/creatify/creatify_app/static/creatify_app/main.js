a0142499ef0433bf1c34d65b4444df0fa0f49942
PontifexMaximus268 <omanreinhart@gmail.com>
2023-02-15 23:14:44 -0600

    updated main.html ui and base.html convention

--- web_dev/creatify/creatify_app/static/creatify_app/main.js
+++ web_dev/creatify/creatify_app/static/creatify_app/main.js
@@ -2,3 +2,16 @@ function toggleButtonClass(button) {
     button.classList.toggle('clicked');
     button.classList.toggle('unclicked');
   }
+  const coll = document.getElementsByClassName("collapsible");
+  
+  for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
--- web_dev/creatify/creatify_app/views.py
+++ web_dev/creatify/creatify_app/views.py
@@ -2,8 +2,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
 def baserender(request):
+    sliders = ['Danceability', 'Energy', 'Loudness', 'Instrumentalness', 'Liveness']
     genres = ['acoustic', 'afrobeat', 'alt-rock', 'alternative', 'ambient', 'anime', 'black-metal', 'bluegrass', 'blues', 'bossanova', 'brazil', 'breakbeat', 'british', 'cantopop', 'chicago-house', 'children', 'chill', 'classical', 'club', 'comedy', 'country', 'dance', 'dancehall', 'death-metal', 'deep-house', 'detroit-techno', 'disco', 'disney', 'drum-and-bass', 'dub', 'dubstep', 'edm', 'electro', 'electronic', 'emo', 'folk', 'forro', 'french', 'funk', 'garage', 'german', 'gospel', 'goth', 'grindcore', 'groove', 'grunge', 'guitar', 'happy', 'hard-rock', 'hardcore', 'hardstyle', 'heavy-metal', 'hip-hop', 'holidays', 'honky-tonk', 'house', 'idm', 'indian', 'indie', 'indie-pop', 'industrial', 'iranian', 'j-dance', 'j-idol', 'j-pop', 'j-rock', 'jazz', 'k-pop', 'kids', 'latin', 'latino', 'malay', 'mandopop', 'metal', 'metal-misc', 'metalcore', 'minimal-techno', 'movies', 'mpb', 'new-age', 'new-release', 'opera', 'pagode', 'party', 'philippines-opm', 'piano', 'pop', 'pop-film', 'post-dubstep', 'power-pop', 'progressive-house', 'psych-rock', 'punk', 'punk-rock', 'r-n-b', 'rainy-day', 'reggae', 'reggaeton', 'road-trip', 'rock', 'rock-n-roll', 'rockabilly', 'romance', 'sad', 'salsa', 'samba', 'sertanejo', 'show-tunes', 'singer-songwriter', 'ska', 'sleep', 'songwriter', 'soul', 'soundtracks', 'spanish', 'study', 'summer', 'swedish', 'synth-pop', 'tango', 'techno', 'trance', 'trip-hop', 'turkish', 'work-out', 'world-music']
-    return render(request, 'creatify_app/main.html', {'genres':genres})
+    return render(request, 'creatify_app/main.html', {'genres':genres, 'sliders':sliders})
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -17,7 +18,6 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'creatify_app/main.html', {'form': form})
-
 
 def login_view(request):
     if request.method == 'POST':
