4005e47c79b1369e904ad12c876baffced0a6184
PontifexMaximus268 <omanreinhart@gmail.com>
2023-02-21 11:31:07 -0600

    dynamic slider name and desc

--- web_dev/creatify/creatify_app/views.py
+++ web_dev/creatify/creatify_app/views.py
@@ -7,7 +7,24 @@ from .running import generate_playlist
 # run notebook 6
 #rendering base page view
 def baserender(request):
-    sliders = ['Danceability', 'Energy', 'Loudness', 'Instrumentalness', 'Liveness']
+    sliders = [
+  {
+    "name": "Danceability",
+    "desc": "Danceability measures how suitable for dancing and is based on musical elements such as tempo, rhythm stability, beat and regularity.",
+  },
+  {
+    "name": "Energy",
+    "desc": "Energy measures the intensity and activity based on loudness, timbre, and other factors."
+  },
+  {
+    "name": "Valence",
+    "desc": "Valence is the positivity of the song i.e. happier songs have higher valence"
+  },
+  {
+    "name": "Instrumentalness",
+    "desc": "Instrumentalness measures how much of the song is predominated with vocals or instruments"
+  }
+]
     genres = ['acoustic', 'afrobeat', 'alt-rock', 'alternative', 'ambient', 'anime', 'black-metal', 'bluegrass', 'blues', 'bossanova', 'brazil', 'breakbeat', 'british', 'cantopop', 'chicago-house', 'children', 'chill', 'classical', 'club', 'comedy', 'country', 'dance', 'dancehall', 'death-metal', 'deep-house', 'detroit-techno', 'disco', 'disney', 'drum-and-bass', 'dub', 'dubstep', 'edm', 'electro', 'electronic', 'emo', 'folk', 'forro', 'french', 'funk', 'garage', 'german', 'gospel', 'goth', 'grindcore', 'groove', 'grunge', 'guitar', 'happy', 'hard-rock', 'hardcore', 'hardstyle', 'heavy-metal', 'hip-hop', 'holidays', 'honky-tonk', 'house', 'idm', 'indian', 'indie', 'indie-pop', 'industrial', 'iranian', 'j-dance', 'j-idol', 'j-pop', 'j-rock', 'jazz', 'k-pop', 'kids', 'latin', 'latino', 'malay', 'mandopop', 'metal', 'metal-misc', 'metalcore', 'minimal-techno', 'movies', 'mpb', 'new-age', 'new-release', 'opera', 'pagode', 'party', 'philippines-opm', 'piano', 'pop', 'pop-film', 'post-dubstep', 'power-pop', 'progressive-house', 'psych-rock', 'punk', 'punk-rock', 'r-n-b', 'rainy-day', 'reggae', 'reggaeton', 'road-trip', 'rock', 'rock-n-roll', 'rockabilly', 'romance', 'sad', 'salsa', 'samba', 'sertanejo', 'show-tunes', 'singer-songwriter', 'ska', 'sleep', 'songwriter', 'soul', 'soundtracks', 'spanish', 'study', 'summer', 'swedish', 'synth-pop', 'tango', 'techno', 'trance', 'trip-hop', 'turkish', 'work-out', 'world-music']
     return render(request, 'creatify_app/main.html', {'genres':genres, 'sliders':sliders})
 # results page view
