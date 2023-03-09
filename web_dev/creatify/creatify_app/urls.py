78c81f5b6e14e74477b188ab51480ec271de1943
PontifexMaximus268 <omanreinhart@gmail.com>
2023-03-09 14:11:44 -0600

    victoria view

--- web_dev/creatify/creatify_app/urls.py
+++ web_dev/creatify/creatify_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('base/', views.baserender, name='base'),
     path('results/', views.result_view, name='result_view'),
     path('authorize/', views.auth_view, name='auth'),
+    path('victoria/', views.vic_view, name='vic_view'),
 ]
--- web_dev/creatify/creatify_app/views.py
+++ web_dev/creatify/creatify_app/views.py
@@ -6,29 +6,7 @@ from . import credentials
 import json
 import pandas as pd
 import requests
-def get_top_features(sp):
-    '''
-    This is a function that
-    '''
-
-    ranges = ['short_term', 'medium_term', 'long_term']
-
-    top_tracks = []
-
-    for sp_range in ranges:
-        results = sp.current_user_top_tracks(time_range=sp_range, limit=50)
-        for i in range(len(results)):
-            top_tracks.append(results['items'][i]['uri'])
-    audio_features = sp.audio_features(top_tracks)
-    df = pd.DataFrame(audio_features)
-    songs = {'danceability': [df.loc[df['danceability'].idxmin(), 'uri'], df.loc[df['danceability'].idxmax(), 'uri']], 
-        'energy': [df.loc[df['energy'].idxmin(), 'uri'], df.loc[df['energy'].idxmax(), 'uri']], 
-         'valence': [df.loc[df['valence'].idxmin(), 'uri'], df.loc[df['valence'].idxmax(), 'uri']],
-         'instrumentalness': [df.loc[df['instrumentalness'].idxmin(), 'uri'], df.loc[df['instrumentalness'].idxmax(), 'uri']],
-         'tempo': [df.loc[df['tempo'].idxmin(), 'uri'], df.loc[df['tempo'].idxmax(), 'uri']]}
-    return songs # this returns the above values in min then max order
-def baserender(request):
-    
+def vic_view(request):
     sliders = [
   {
     "name": "Danceability",
@@ -68,6 +46,68 @@ def baserender(request):
 ]
     genres = ['afrobeat', 'alt-Rock', 'alternative', 'ambient', 'anime', 'black-Metal', 'bluegrass', 'blues', 'bossanova', 'brazil', 'breakbeat', 'british', 'cantopop', 'chicago-House', 'children', 'chill', 'classical', 'club', 'comedy', 'country', 'dance', 'dancehall', 'death-Metal', 'deep-House', 'detroit-Techno', 'disco', 'disney', 'drum-and-Bass', 'dub', 'dubstep', 'edm', 'electro', 'electronic', 'emo', 'folk', 'forro', 'french', 'funk', 'garage', 'german', 'gospel', 'goth', 'grindcore', 'groove', 'grunge', 'guitar', 'happy', 'hard-Rock', 'hardcore', 'hardstyle', 'heavy-Metal', 'hip-Hop', 'holidays', 'honky-Tonk', 'house', 'idm', 'indian', 'indie', 'indie-Pop', 'industrial', 'iranian', 'j-Dance', 'j-Idol', 'j-Pop', 'j-Rock', 'jazz', 'k-Pop', 'kids', 'latin', 'latino', 'malay', 'mandopop', 'metal', 'metal-Misc', 'metalcore', 'minimal-Techno', 'movies', 'mpb', 'new-Age', 'new-Release', 'opera', 'pagode', 'party', 'philippines-Opm', 'piano', 'pop', 'pop-Film', 'post-Dubstep', 'power-Pop', 'progressive-House', 'psych-Rock', 'punk', 'punk-Rock', 'R-N-B', 'rainy-Day', 'reggae', 'reggaeton', 'road-trip', 'rock', 'rock-n-Roll', 'rockabilly', 'romance', 'sad', 'salsa', 'samba', 'sertanejo', 'show-Tunes', 'singer-Songwriter', 'ska', 'sleep', 'songwriter', 'soul', 'soundtracks', 'spanish', 'study', 'summer', 'swedish', 'synth-Pop', 'tango', 'techno', 'trance', 'trip-Hop', 'turkish', 'work-Out', 'world-Music']
     return render(request, 'creatify_app/victoria.html', {'genres':genres, 'sliders':sliders})
+def get_top_features(sp): # fix so it can take a token instead
+    '''
+    This is a function that
+    '''
+
+    ranges = ['short_term', 'medium_term', 'long_term']
+
+    top_tracks = []
+
+    for sp_range in ranges:
+        results = sp.current_user_top_tracks(time_range=sp_range, limit=50)
+        for i in range(len(results)):
+            top_tracks.append(results['items'][i]['uri'])
+    audio_features = sp.audio_features(top_tracks)
+    df = pd.DataFrame(audio_features)
+    songs = {'danceability': [df.loc[df['danceability'].idxmin(), 'uri'], df.loc[df['danceability'].idxmax(), 'uri']], 
+        'energy': [df.loc[df['energy'].idxmin(), 'uri'], df.loc[df['energy'].idxmax(), 'uri']], 
+         'valence': [df.loc[df['valence'].idxmin(), 'uri'], df.loc[df['valence'].idxmax(), 'uri']],
+         'instrumentalness': [df.loc[df['instrumentalness'].idxmin(), 'uri'], df.loc[df['instrumentalness'].idxmax(), 'uri']],
+         'tempo': [df.loc[df['tempo'].idxmin(), 'uri'], df.loc[df['tempo'].idxmax(), 'uri']]}
+    return songs # this returns the above values in min then max order
+def baserender(request): #add in top features function
+    #add in top features function
+    sliders = [
+  {
+    "name": "Danceability",
+    "desc": "Danceability measures how suitable for dancing and is based on musical elements such as tempo, rhythm stability, beat and regularity.",
+    "low":"Low",
+    "medium":"Medium",
+    "high":"High"
+  },
+  {
+    "name": "Energy",
+    "desc": "Energy measures the intensity and activity based on loudness, timbre, and other factors.",
+    "low":"Calmer",
+    "medium":"Average",
+    "high":"Energetic"
+  },
+  {
+    "name": "Valence",
+    "desc": "Valence is the positivity of the song i.e. happier songs have higher valence",
+    "low":"Meloncholic",
+    "medium":"Average",
+    "high":"Cheery"
+  },
+  {
+    "name": "Instrumentalness",
+    "desc": "Instrumentalness measures how much of the song is predominated with vocals or instruments",
+    "low":"Vocal",
+    "medium":"Average",
+    "high":"Instrumental"
+  },
+  {
+    "name": "Tempo",
+    "desc": "The speed of the track in beats per minute",
+    "low":"Slower",
+    "medium":"Average",
+    "high":"Faster"
+  }
+]
+    genres = ['afrobeat', 'alt-Rock', 'alternative', 'ambient', 'anime', 'black-Metal', 'bluegrass', 'blues', 'bossanova', 'brazil', 'breakbeat', 'british', 'cantopop', 'chicago-House', 'children', 'chill', 'classical', 'club', 'comedy', 'country', 'dance', 'dancehall', 'death-Metal', 'deep-House', 'detroit-Techno', 'disco', 'disney', 'drum-and-Bass', 'dub', 'dubstep', 'edm', 'electro', 'electronic', 'emo', 'folk', 'forro', 'french', 'funk', 'garage', 'german', 'gospel', 'goth', 'grindcore', 'groove', 'grunge', 'guitar', 'happy', 'hard-Rock', 'hardcore', 'hardstyle', 'heavy-Metal', 'hip-Hop', 'holidays', 'honky-Tonk', 'house', 'idm', 'indian', 'indie', 'indie-Pop', 'industrial', 'iranian', 'j-Dance', 'j-Idol', 'j-Pop', 'j-Rock', 'jazz', 'k-Pop', 'kids', 'latin', 'latino', 'malay', 'mandopop', 'metal', 'metal-Misc', 'metalcore', 'minimal-Techno', 'movies', 'mpb', 'new-Age', 'new-Release', 'opera', 'pagode', 'party', 'philippines-Opm', 'piano', 'pop', 'pop-Film', 'post-Dubstep', 'power-Pop', 'progressive-House', 'psych-Rock', 'punk', 'punk-Rock', 'R-N-B', 'rainy-Day', 'reggae', 'reggaeton', 'road-trip', 'rock', 'rock-n-Roll', 'rockabilly', 'romance', 'sad', 'salsa', 'samba', 'sertanejo', 'show-Tunes', 'singer-Songwriter', 'ska', 'sleep', 'songwriter', 'soul', 'soundtracks', 'spanish', 'study', 'summer', 'swedish', 'synth-Pop', 'tango', 'techno', 'trance', 'trip-Hop', 'turkish', 'work-Out', 'world-Music']
+    return render(request, 'creatify_app/base.html', {'genres':genres, 'sliders':sliders})
 # results page view
 def result_view(request):
     if request.method == 'POST':
@@ -148,7 +188,6 @@ def auth_view(request):
         # If we don't have a token in the session, redirect the user to Spotify authorization page.
         auth_url = sp_oauth.get_authorize_url()
         return redirect(auth_url)
-
 # Login and Logout Views
 def register(request):
     if request.method == 'POST':
