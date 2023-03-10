e47505772fa6edc5f7dc351f1440336cdbc1e8a3
PontifexMaximus268 <omanreinhart@gmail.com>
2023-03-10 00:08:53 -0600

    changed urls convention

--- web_dev/creatify/creatify/urls.py
+++ web_dev/creatify/creatify/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("creatify_app.urls"))
+    path('', include("creatify_app.urls", namespace='creatify')),
 ]
